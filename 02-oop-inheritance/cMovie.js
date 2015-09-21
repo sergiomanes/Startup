b82da89099e073df598a6615336ccb8253984ce8
sergiomanes <manes.sergio@gmail.com>
2015-09-21 18:12:46 -0300

    Update cMovie.js

--- 02-oop-inheritance/cMovie.js
+++ 02-oop-inheritance/cMovie.js
@@ -1,35 +1,40 @@
 //CLASE MOVIE
-function movie () {
+function Movie () {
   //atributos
-  attributes = {};
-  attributes["title"]=" ";
-  observers= new movieObserver();
-  
-  //metodos
-  this.play= function() {
-   var observerCount = observers.count();
-   for(var i=0; i < observerCount; i++){
-   observers.get(i).update( attributes["title"].value + " is playing..." );
-   }
+  this.attributes = {
+    title: ""
   }
 
-  this.stop= function() {
-    var observerCount = observers.count();
-    for(var i=0; i < observerCount; i++){
-    observers.get(i).update( attributes["title"].value + " is stopped" );
-    }
-  }
-
-  this.set= function(attr, value) {
-    attributes[attr]={value};
-  }
-
-  this.get= function(attr) {
-    var nombre = attriubutes[attr];
-    console.log(nombre);
-  }
-  
-  this.addObserver = function( observer ){
-                    observers.add( observer );
-                }
+  this.observers = new Observer(); 
 }
+
+
+//metodos
+Movie.prototype.play= function() {
+ this.updateObserver(this.attributes.title + " is playing...");
+}
+
+Movie.prototype.stop= function() {    
+  this.updateObserver(this.attributes.title + " is stopped");
+}
+
+Movie.prototype.set= function(attr, value) {
+  this.attributes[attr]=value;
+}
+
+Movie.prototype.get= function(attr) {
+  var nombre = this.attriubutes[attr];
+  console.log(nombre);
+}
+
+Movie.prototype.addObserver = function( observer ){
+    this.observers.add( observer );
+}
+
+Movie.prototype.updateObserver = function (msg){
+  var observerCount = this.observers.count();
+  for(var i=0; i < observerCount; i++){
+    this.observers.get(i).update(msg);
+  }
+}
+
