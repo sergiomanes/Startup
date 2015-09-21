af86a50ca324d1151f8af8114d31b35d0733eccc
sergiomanes <manes.sergio@gmail.com>
2015-09-21 18:13:16 -0300

    Update cMovieObserver.js

--- 02-oop-inheritance/cMovieObserver.js
+++ 02-oop-inheritance/cMovieObserver.js
@@ -1,8 +1,5 @@
 function movieObserver(){}
 
-  movieObserver.prototype= new Observer();
-  movieObserver.prototype.constructor=movieObserver;
-
   movieObserver.prototype.update= function(text){
     console.log(text);
     
